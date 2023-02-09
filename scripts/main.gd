b279fe7f8d7322088083e79cc409c91eed0d487b
Ivan Datsko <miturtow@gmail.com>
2023-02-09 23:15:13 +0200

    Add code to create buttons and assign texture slices to them

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,12 +1,18 @@
 extends Node
 
 func _ready():
-	var tex: Texture = load("res://assets/img/cat.png")
+	var image: Image = Image.new()
+	var tex: ImageTexture = ImageTexture.new()
+	image.load("res://assets/img/cat.png")
+	tex.create_from_image(image)
+	tex.set_size_override(Vector2(255, 255))
 	var tex_slices: Array = slice_texture(tex, 3, 3)
 	for slice in tex_slices:
-		print_debug(slice.region)
-
-func slice_texture(tex: Texture, cols: int, rows: int) -> Array:
+		var tex_button: TextureButton = TextureButton.new()
+		tex_button.texture_normal = slice
+		$GridContainer.add_child(tex_button)
+	
+func slice_texture(tex: ImageTexture, cols: int, rows: int) -> Array:
 	var result: Array = []
 	var region: Rect2 = Rect2(Vector2(0, 0), Vector2(tex.get_width() / cols, tex.get_height() / rows))
 	var offset_x: bool = false
